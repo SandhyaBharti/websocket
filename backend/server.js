d5cbfabc6aabed66b70c50ec379bc4f3f84c96f0
Sandhya Bharti <sandhyabharti298@gmail.com>
2026-02-18 11:38:49 +0530

    Removed node_modules

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const PORT = 4000;
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+// hashmap: we will store users data 
+const users = new Map();
+
+io.on('connection', (socket) => {
+    console.log("User connected", socket.id)
+
+    // socket.on("send_message", socket.id);
+
+    socket.on("send_message", (message)  => {
+        const newMessage = {
+            from: message.userName,
+            time: new Date(Date.now()).toLocaleString(),
+            message: message.data
+        }
+        io.to(socket.id).emit("received_message", newMessage);
+    });
+    socket.on("join", (userName) => {
+        users.set(userName, socket.id);
+        io.emit("users", Array.from(users.keys()));
+    });
+    socket.on("disconnect", () => {
+        users.delete(socket.id);
+        io.emit("users", Array.from(users.keys()));
+    })
+});
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Guys!</h1>');
+});
+
+server.listen(PORT, () => console.log(`Server is listening at http://localhost:${PORT}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,66 @@
+import socket from './utils/socket.js';
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  const SEND_MESSAGE = "send_message";
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [isConnected, setIsConnected] = useState(false);
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    socket.on("received_message", (message) => {
+      setMessages((prev) => [...prev, message])
+    })
+    socket.on("users", (user) => {
+      setUsers((prev) => [...prev, message])
+    });
+
+    return () => {
+      socket.off("received_message");
+      socket.off("users");
+    }
+
+  }, [])
+
+  const sendMessage = () => {
+    socket.emit(SEND_MESSAGE, {
+      userName,
+      data: "Hello",
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!userName) return;
+    socket.emit("join", userName);
+  }
+
+  return <div>
+    {!isConnected && (
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          minLength={3}
+          required />
+        <button>Join Chat</button>
+      </form>
+    )}
+
+
+    {
+      messages?.length === 0 ? (
+        <p>No messages</p>
+      ) : (
+        messages?.map((msg, idx) => (
+          <div key={idx}>{msg.message} : {msg.from} : {msg.time} </div>
+        ))
+      )
+    }
+    <button onClick={sendMessage}>Send Message</button>
+  </div>
+
+}
+
+export default App
